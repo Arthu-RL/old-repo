08e306d3ca52aea32ede14e3bde8fcffc2c878ad
ArthurRL <111669311+Arthu-RL@users.noreply.github.com>
2023-03-06 13:42:19 -0300

    Update Finding big prime numbers.py

--- Finding big prime numbers.py
+++ Finding big prime numbers.py
@@ -1,24 +1,26 @@
 x = int(input("Put the number you want to discover if it is prime: "))
 cont = 2
 a = "That is not a prime number!"
+r = x % cont
 
-while True:
-    y = x % cont
 
-    if y != 0 and cont <= (x/2):
-        print(f'The remainder of {x} divided by {cont} is {y}')
-    elif y == 0 and cont <= (x/2):
-        print(a)
-        print(f'Because the remainder of {x} divided by {cont} is {y}')
-        break
-    elif x == 2:
-        print("YOU FIND A PRIME NUMBER!!!")
-        break
-    elif y == 0 or x == 1:
-        print(a)
-        break
-    else:
-        print("YOU FIND A PRIME NUMBER!!!")
-        break
+if x == 2:
+    print("YOU FIND A PRIME NUMBER!!!")
+elif r == 0 or x == 1:
+    print(a)
+else:
+    cont = 3
+    while True:
+        r = x % cont
 
-    cont += 1
+        if r != 0 and cont <= (x/2):
+            print(f'The remainder of {x} divided by {cont} is {r}')
+        elif r == 0 and cont <= (x/2):
+            print(a)
+            print(f'Because the remainder of {x} divided by {cont} is {r}')
+            break
+        else:
+            print("YOU FIND A PRIME NUMBER!!!")
+            break
+
+        cont += 2
